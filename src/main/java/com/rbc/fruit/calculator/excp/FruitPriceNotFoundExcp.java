02c91f29fdd8597102955e6057b0ef873e44330c
sguharoy <116178415+sguharoy@users.noreply.github.com>
2022-10-19 22:34:56 +0530

    Update FruitPriceNotFoundExcp.java
    Removed unnecessary methods

--- src/main/java/com/rbc/fruit/calculator/excp/FruitPriceNotFoundExcp.java
+++ src/main/java/com/rbc/fruit/calculator/excp/FruitPriceNotFoundExcp.java
@@ -5,18 +5,11 @@ package com.rbc.fruit.calculator.excp;
  */
 public class FruitPriceNotFoundExcp extends RuntimeException{
 
-    public FruitPriceNotFoundExcp(){
+   public FruitPriceNotFoundExcp(){
 
     }
     public FruitPriceNotFoundExcp(final String message) {
         super(message);
-    }
-    public FruitPriceNotFoundExcp(final Throwable cause) {
-        super(cause);
-    }
-
-    public FruitPriceNotFoundExcp(final String message, final Throwable cause) {
-        super(message, cause);
     }
 
 
